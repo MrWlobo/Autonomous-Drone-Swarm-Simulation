617a51bac1ee59b841989b0562fb232df7dde1b8
QnK1 <kacperro03@gmail.com>
2025-11-26 15:04:27 +0100

    fixed error

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mesa.experimental.devs import ABMSimulator
 from mesa.visualization import SolaraViz
 from model.model import DroneModel
-from visualization.viz import VisualizationComponent
+from visualization.viz import VisualizationComponent, Layout # noqa: F401
 
 
 model_params = {
